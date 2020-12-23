4e91e66f75b7f8dd01c2425a26cbd374dcdbb317
leonardovalverderadarfit <70704988+leonardovalverderadarfit@users.noreply.github.com>
2020-12-23 12:11:34 -0300

    feat: node rest apis + reactjs rest

--- src/pages/users/index.tsx
+++ src/pages/users/index.tsx
@@ -1,10 +1,9 @@
-import { GetStaticProps } from 'next'
+import { GetServerSideProps } from 'next'
 import Link from 'next/link'
-
 import { User } from '../../interfaces'
-import { sampleUserData } from '../../utils/sample-data'
 import Layout from '../../components/Layout'
 import List from '../../components/List'
+import axios from 'axios'
 
 type Props = {
   items: User[]
@@ -14,7 +13,7 @@ const WithStaticProps = ({ items }: Props) => (
   <Layout title="Users List | Next.js + TypeScript Example">
     <h1>Users List</h1>
     <p>
-      Example fetching data from inside <code>getStaticProps()</code>.
+      Example fetching data from inside <code>getServerSideProps()</code>.
     </p>
     <p>You are currently on: /users</p>
     <List items={items} />
@@ -26,11 +25,12 @@ const WithStaticProps = ({ items }: Props) => (
   </Layout>
 )
 
-export const getStaticProps: GetStaticProps = async () => {
+export const getServerSideProps: GetServerSideProps = async () => {
   // Example for including static props in a Next.js function component page.
   // Don't forget to include the respective types for any props passed into
   // the component.
-  const items: User[] = sampleUserData
+  const response = await axios.get('http://localhost:3000/api/users');
+  const items: User[] = await response.data;
   return { props: { items } }
 }
 
